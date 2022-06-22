1e518728b97a1743786a2c4acf7c8d2447576b07
Andy Dodge <andrew.andy.dodge@gmail.com>
2022-06-21 23:00:36 -0500

    feat: added file metadata service

---
+++ src/API.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import multer from 'multer';
+
+export class API {
+    static start() {
+        const app = express();
+        const upload = multer({ dest: path.join(__dirname, '../uploads/') });
+        const port = process.env.PORT || 80; // Specific to Heroku
+        app.use(express.json());
+        app.use(cors({ optionsSuccessStatus: 200 })); // Specific to freeCodeCamp projects
+
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, '../public/index.html'));
+        });
+
+        app.post('/api/upload', upload.single('upfile'), (req, res) => {
+            console.log(req.file);
+            res.json({
+                name: req.file?.originalname,
+                type: req.file?.mimetype,
+                size: req.file?.size,
+            });
+        });
+
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`);
+        });
+    }
+}
